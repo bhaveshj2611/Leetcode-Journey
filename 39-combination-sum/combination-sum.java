7c70be109f67b9a117464ac7273a0d54d70d9a80
Bhavesh Joshi <91772975+bhaveshj2611@users.noreply.github.com>
2024-01-18 11:22:55 +0530

    Time: 4 ms (22.49%) | Memory: 44.9 MB (12.92%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,20 +1,33 @@
 class Solution {
    public List<List<Integer>> combinationSum(int[] nums, int target) {
     List<List<Integer>> list = new ArrayList<>();
+    List<Integer> ans = new ArrayList<>();
     Arrays.sort(nums);
-    backtrack(list, new ArrayList<>(), nums, target, 0);
+    backtrack(list, ans , nums, target, 0,0);
     return list;
 }
-
-private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
-    if(remain < 0) return;
-    else if(remain == 0) list.add(new ArrayList<>(tempList));
-    else{ 
-        for(int i = start; i < nums.length; i++){
-            tempList.add(nums[i]);
-            backtrack(list, tempList, nums, remain - nums[i], i); // not i + 1 because we can reuse same elements
-            tempList.remove(tempList.size() - 1);
+    public void backtrack(List<List<Integer>> list, List<Integer> ans, int[] nums, int target, int sum, int index ){
+        
+        if(sum> target){
+            return;
         }
+
+        if(index == nums.length){
+            if(sum == target){
+                list.add(new ArrayList<>(ans));
+            }
+            return;
+        }
+
+
+        sum = sum + nums[index];
+        ans.add(nums[index]);
+        backtrack(list,ans,nums,target,sum,index);
+        ans.remove(ans.size()-1);
+        sum = sum - nums[index];
+
+
+        backtrack(list,ans,nums,target,sum,index+1);
     }
-}
+
 }
