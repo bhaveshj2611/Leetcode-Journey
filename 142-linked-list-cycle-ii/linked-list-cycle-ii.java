2b4574019e94d8287e3855aabee9565f3aef2e53
Bhavesh Joshi <91772975+bhaveshj2611@users.noreply.github.com>
2024-01-19 15:47:56 +0530

    Time: 1 ms (33.03%) | Memory: 44.5 MB (26.60%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -11,17 +11,54 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        HashSet<ListNode> set =new HashSet<>();
-        ListNode temp = head;
-        while(temp!=null){
-            if(!set.contains(temp)){
-                set.add(temp);
-                temp = temp.next;
-            }
-            else {
-                return temp;
+         ListNode fast = head;
+        ListNode slow = head;
+        int length = 0;
+        while(fast!=null && fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                length = lengthCycle(slow);
+                break;
             }
         }
-        return null;
+        if(length==0){
+            return null;
+        }
+        ListNode first = head;
+        ListNode second = head;
+        while(length>0){
+            second = second.next;
+            length--;
+        }
+
+        while(first!=second){
+            second = second.next;
+            first = first.next;
+        }
+        return second;
+    }
+
+    public int lengthCycle(ListNode head){
+         ListNode fast = head;
+        ListNode slow = head;
+        // int length = 0;
+        while(fast!=null && fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                ListNode temp= slow;
+                int length = 0;
+                do{
+                    temp = temp.next;
+                    length++;
+                }
+                while(temp!=slow);
+                System.out.println(length);
+                return length;
+            }
+        }
+        // System.out.println(length);
+        return 0;
     }
 }
