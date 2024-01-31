77025ac8919ea945203f022e75bb9ec010680b3f
Bhavesh Joshi <91772975+bhaveshj2611@users.noreply.github.com>
2024-01-31 13:37:39 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (27.91%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -1,68 +1,63 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode temp1 = list1;
-        ListNode temp2 = list2;
-        ListNode head3= null;
-        ListNode temp3 = head3;
-        while(temp1!=null && temp2!=null){
-            if(temp1.val<=temp2.val){
-                ListNode node = new ListNode(temp1.val);
-                if(head3==null){
-                    head3 = node;
-                }
-                else {
-                    temp3.next = node;
-                }
-                temp3 = node;
-                temp1 = temp1.next;
-                // temp1.next = temp1;
-                // temp1 = temp1.next;
-                
+        // ListNode ans = new ListNode(0);
+        // ListNode node = ans;
+        // while (list1 != null && list2 != null) {
+        //     if (list1.val < list2.val) {
+        //         node.next = list1;
+        //         list1 = list1.next;
+        //         node = node.next;
+        //     } else {
+        //         node.next = list2;
+        //         list2 = list2.next;
+        //         node = node.next;
+        //     }
+        // }
+        // if (list1 != null) {
+        //     node.next = list1;
+        // } else {
+        //     node.next = list2;
+        // }
+
+        // return ans.next;
+
+          ListNode dummy = new ListNode(0);
+        ListNode temp = dummy;
+        while(list1!=null && list2!=null){
+            if(list1.val<list2.val){
+                temp.next = list1;
+                temp = temp.next;
+                list1 = list1.next;
             }
-            else if(temp2.val<temp1.val){
-                ListNode node = new ListNode(temp2.val);
-                if(head3==null){
-                    head3 = node;
-                }
-                else {
-                    temp3.next = node;
-                }
-                temp3 = node;
-                temp2 = temp2.next;
+            else{
+                temp.next = list2;
+                temp = temp.next;
+                list2 = list2.next;
             }
         }
 
-        while(temp1!=null){
-            ListNode node = new ListNode(temp1.val);
-            if(head3 == null){
-                head3 = node;
-            }
-            else temp3.next = node;
-            temp3 = node;
-            temp1 = temp1.next;
+        while(list1==null && list2!=null){
+            temp.next = list2;
+            list2 = list2.next;
+            temp = temp.next;
+
+        }
+        while(list2==null && list1!=null){
+            temp.next = list1;
+            list1 = list1.next;
+            temp = temp.next;
         }
 
-        
-        while(temp2!=null){
-            ListNode node = new ListNode(temp2.val);
-            if(head3 == null){
-                head3 = node;
-            }
-            else temp3.next = node;
-            temp3 = node;
-            temp2 = temp2.next;
-        }
-
-        return head3;
+        return dummy.next;
     }
 }
