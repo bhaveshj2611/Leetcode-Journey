a72099679ad1c0f61b21b6a37f87661046d42ccf
Bhavesh Joshi <91772975+bhaveshj2611@users.noreply.github.com>
2024-02-07 09:23:42 +0530

    Time: 7 ms (41.34%) | Memory: 68.8 MB (20.52%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,65 +10,46 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-    Stack<ListNode> st = new Stack<>();
     
-    ListNode temp = head;
-    while(temp!=null){
-        st.push(temp);
-        temp = temp.next;
+    if(head == null || head.next ==null){
+        return true;
     }
 
-    // ListNode mid = middle(head);
-    // ListNode head2 = mid.next;
-    while(!st.isEmpty() && head != null){
-        if(st.pop().val!=head.val){
+    ListNode mid = middle(head);
+    ListNode revHead = reverseList(mid);
+    // mid.next = null;
+
+
+    while(head!=null && revHead !=null){
+        if(head.val != revHead.val){
             return false;
         }
-        else{
-            head = head.next;
-        }
+        head = head.next;
+        revHead = revHead.next;
     }
     return true;
-    
-    // if(head == null || head.next ==null){
-    //     return true;
-    // }
-
-    // ListNode mid = middle(head);
-    // ListNode revHead = reverseList(mid);
-    // // mid.next = null;
-
-
-    // while(head!=null && revHead !=null){
-    //     if(head.val != revHead.val){
-    //         return false;
-    //     }
-    //     head = head.next;
-    //     revHead = revHead.next;
-    // }
-    // return true;
 
 }
 
-//  public ListNode middle(ListNode head){
-//      ListNode fast = head;
-//      ListNode slow = head;
-//      while(fast!=null && fast.next !=null){
-//          fast = fast.next.next;
-//          slow = slow.next;
-//      }
-//      return slow;
-//  }
+ public ListNode middle(ListNode head){
+     ListNode fast = head;
+     ListNode slow = head;
+     while(fast!=null && fast.next !=null){
+         fast = fast.next.next;
+         slow = slow.next;
+     }
+     return slow;
+ }
 
-//  public ListNode reverseList(ListNode head){
-//      ListNode prev = null;
-//      ListNode pres = head;
-//      while(pres!=null){
-//          ListNode next = pres.next;
-//          pres.next = prev;
-//          prev = pres;
-//          pres = next;
-//      }
-//      return prev;
-//  }
+ public ListNode reverseList(ListNode head){
+     ListNode prev = null;
+     ListNode pres = head;
+     while(pres!=null){
+         ListNode next = pres.next;
+         pres.next = prev;
+         prev = pres;
+         pres = next;
+     }
+     return prev;
+ }
 }
