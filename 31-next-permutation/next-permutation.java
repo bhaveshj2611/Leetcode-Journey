d65a2b0fba20944dce586b518bd2c2b8c9e41b4e
Bhavesh Joshi <91772975+bhaveshj2611@users.noreply.github.com>
2024-02-09 14:32:41 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (66.18%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,41 +1,50 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n = nums.length;
-        int idx = -1;
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                idx = i;
-                break;
-            }
-        }
-        if(idx == -1){
-            reverse(nums,0);
-            return;
+    int idx = -1;
+    for(int i=nums.length-2;i>=0;i--){
+        if(nums[i]<nums[i+1]){
+            idx = i;
+            break;
         }
         
-        int minMax = -1;
-        for(int i=n-1;i>=0;i--){
-            if(nums[i]>nums[idx]){
-                minMax = i;
-                break;
-            }
-        }
-        swap(nums,idx,minMax);
-        reverse(nums,idx+1);
+    }
+    // System.out.println(idx);
+
+    if(idx==-1){
+        reverse(nums,0);
+        return;
+        
     }
 
-     void swap(int[] nums,int i,int j){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
+    reverse(nums,idx+1);
+    // System.out.println(Arrays.toString(nums));
+    for(int i=idx+1;i<nums.length;i++){
+        if(nums[i]>nums[idx]){
+            swap(nums, i, idx);
+             break;
+        }
+       
     }
-    void reverse(int[] nums,int start){
-        int i=start;
-        int j=nums.length-1;
+    // System.out.println(Arrays.toString(nums));
+
+
+    }
+
+
+    public void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+
+    public void reverse(int[] nums,int start){
+        int i= start;
+        int j = nums.length-1;
         while(i<j){
-            swap(nums,i,j);
+            swap(nums, i, j);
             i++;
             j--;
-        }
-    }
+        }}
+    
 }
