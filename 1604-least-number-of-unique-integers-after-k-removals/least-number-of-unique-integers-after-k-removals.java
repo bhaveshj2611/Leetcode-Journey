8030e2a7934a768ba705277e28be2f43b03f3dc5
Bhavesh Joshi <91772975+bhaveshj2611@users.noreply.github.com>
2024-02-17 00:43:15 +0530

    Time: 48 ms (69.11%) | Memory: 63.3 MB (21.66%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.java
@@ -5,60 +5,16 @@ class Solution {
         for(int val : arr){
             map.put(val,map.getOrDefault(val,0)+1);
         }
-        // PriorityQueue<Integer> pq = new PriorityQueue<>(count.values());
-        // while(k>0){
-
-        // }
-        List<Integer> list = new ArrayList<>();
-        for(int key : map.keySet()){
-            int val = map.get(key);
-            list.add(val);
+        PriorityQueue<Integer> pq = new PriorityQueue<>(map.values());
+        while(k>0){
+            k = k - pq.poll();
         }
+       
 
-        Collections.sort(list);
-        //  System.out.println(list);
-        int j=0;
-         while(k>0){
-             if(list.get(j) == 1){
-                 list.set(j,0);
-                 k--;
-                 j++;
-             }
-             else if(list.get(j) > 1){
-                 list.set(j,list.get(j)-1);
-                 k--;
-             }
-         }
-        // for(int i=0;i<list.size();i++){
-        //     if(k==0){
-        //         break;
-        //     }
-        //     if(list.get(i) == 1){
-        //         list.set(i,0);
-        //         k = k - list.get(i);
-        //     }
-        //     else{
-        //         list.set(i, list.get(i) - 1);
-        //         k = k - list.get(i);
-        //     }
-        // }
-        int ans = 0;
-        for(int i=0;i<list.size();i++){
-            if(list.get(i)!=0){
-                ans++;
-            }
+        if(k<0){
+            return pq.size()+1;
         }
-        // System.out.println(list);
-        //   System.out.println(list.size());
-        // int count = map.size();
-        // int init = 1;
-        // int ans = 0;
-        // int i=0;
-        
-        
-        
-        
-return ans;
+        else return pq.size();
         
     }
 }
