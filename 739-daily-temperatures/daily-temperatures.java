74c0018a0105c9e0a44870bf8ed2c324646e040a
Bhavesh Joshi <91772975+bhaveshj2611@users.noreply.github.com>
2024-03-02 11:33:39 +0530

    Time: 82 ms (22.44%) | Memory: 60.1 MB (61.89%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -1,65 +1,22 @@
 class Solution {
 
-     private class MyPair{
-        private int first;
-        private int second;
-        public MyPair(int first, int second) {
-            this.first = first;
-            this.second = second;
-        }
-	}
-
     public int[] dailyTemperatures(int[] temperatures) {
-        ArrayList<Integer> list = new ArrayList<>();
+        int[] result = new int[temperatures.length];
+        Stack<Integer> st = new Stack<>();
         int n = temperatures.length;
-    
-        Stack<MyPair> st = new Stack<>();
-       
-        for(int i = n - 1;i>=0;i--){
-            if(st.isEmpty()){
-                list.add(-1);
-            }
-            else if(!st.isEmpty() && st.peek().first > temperatures[i]){
-                list.add(st.peek().second);
-            }
-            else{
-                if(st.isEmpty()){
-                    list.add(-1);
-                }
-                else{
-                    while(!st.isEmpty() && st.peek().first <= temperatures[i]){
-                        st.pop();
-                    }
-                    if(st.isEmpty()){
-                        list.add(-1);
-                    }
-                    else{
-                        list.add(st.peek().second);
-                    }
-                }
-                
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && temperatures[i] >= temperatures[st.peek()]) {
+                st.pop();
             }
 
-           
-                 st.push(new MyPair(temperatures[i],i));
-         
-            
-        
+            if (st.isEmpty()) {
+                result[i] = 0;
+            } else {
+                result[i] = st.peek() - i;
+            }
+
+            st.push(i);
         }
-
-        Collections.reverse(list);
-        
-
-        int[] ans = new int[n];
-
-        for(int k=0;k<ans.length;k++){
-            if(list.get(k) != -1){
-                ans[k] = list.get(k) - k;
-            }
-            else{
-                ans[k] = 0;
-            }
-        }
-        return ans;
+        return result;
     }
 }
