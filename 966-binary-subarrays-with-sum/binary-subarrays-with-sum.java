bd484428e1c36122ea0da50fa7158f35e0116eac
Bhavesh Joshi <91772975+bhaveshj2611@users.noreply.github.com>
2024-03-15 14:01:08 +0530

    Time: 22 ms (43.02%) | Memory: 52.8 MB (5.13%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -1,38 +1,18 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-       int i=0;
-       int j=0;
-        int sum = 0;
-        int ans = 0;
-        int countZeros =0;
-       while(j<nums.length){
-        sum = sum + nums[j];
+     
+     Map<Integer,Integer> map = new HashMap<>();
+    int sum = 0;
+    int ans = 0;
+    map.put(0,1);
+     for(int n : nums){
+        sum = sum + n;
 
-
-        while(i<j && (sum>goal || nums[i]==0)){
-                    if(nums[i]==0){
-                        countZeros++;
-                    }
-                    else{
-                        countZeros=0;
-                    }
-                    sum = sum - nums[i];
-                    i++;
-
-                }
-
-                
-        
-            if(sum == goal){
-                ans = ans + 1 + countZeros;
-        
-            }
-
-            j++;
-            
-                
-            
-       }
-       return ans; 
-    }
+        if(map.containsKey(sum-goal)){
+            ans = ans + map.get(sum-goal);
+        }
+        map.put(sum,map.getOrDefault(sum,0)+1);
+     }
+     return ans;
+     }
 }
